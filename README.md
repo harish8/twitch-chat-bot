cde4b716e089266224eb4446cce6a49764f76af5
Harish <7906749+harish8@users.noreply.github.com>
2025-12-19 00:50:49 -0500

    Implement basic bot logic with twitch version 2.9

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Twitch Chat Bot with Web Dashboard.
+# Twitch Stream Assistant with Web Dashboard.
 
-Building a twitch chatbot with a web dashboard that listens and responds to chat with the support of custom commands set by the streamer (eg `!hello`, `!save`, `!bravo`) 
+Building a twitch stream assistant with a web dashboard that listens and responds to chat with the support of custom commands set by the streamer (eg `!hello`, `!save`, `!bravo`) 
 
 ## Tech stack
 * Bot: `Python , twitchio`
--- bot.py
+++ bot.py
@@ -1,24 +1,51 @@
 from dotenv import load_dotenv
+from twitchio.ext import commands
+import logging
+# import httpx
 import os
-import httpx
 
 # load variables from .env
 load_dotenv() 
 
+LOGGER: logging.Logger = logging.getLogger(__name__)
+
+
 client_id = os.getenv("TWITCH_CLIENT_ID")
 client_secret = os.getenv("TWITCH_CLIENT_SECRET")
 redirect_uri = os.getenv("REDIRECT_URI")
+bot_id = os.getenv("BOT_ID")
 
-async def fetch_token():
-    async with httpx.AsyncClient() as client:
-        response = await client.post(
-            "https://id.twitch.tv/oauth2/token",
-            params={
-                "client_id": client_id,
-                "client_secret": client_secret,
-                "grant_type": "client_credentials"
-            }
+class Bot(commands.Bot):
+    def __init__(self):
+        super().__init__(
+            token=f"oauth:{os.getenv('TWITCH_OAUTH_TOKEN')}",
+            prefix="!",
+            initial_channels=["horionx88"]
         )
-        response.raise_for_status()
-        data = response.json()        
-        return data["access_token"]
+
+    async def event_ready(self):
+        print(f"Logged in as | {self.nick}")
+        print(f"User id is | {self.user_id}")
+
+    async def event_message(self, message):
+        if message.echo:
+            return
+        
+        # print(f"DEBUG: Got message: {message.content} from {message.author.name}")
+        print(f"{message.author.name}: {message.content}")  # prints chat to console
+        
+        # await message.channel.send("Bot received your message!")  # <-- sends a message to the channel
+        await self.handle_commands(message)
+
+    @commands.command(name="ping")
+    async def ping(self, ctx):
+        await ctx.send("Pong!")
+
+    @commands.command(name="hello")
+    async def hello(self, ctx):
+        await ctx.send(f"Hello {ctx.author.name}!")
+
+
+if __name__ == '__main__':
+    bot = Bot()
+    bot.run()
